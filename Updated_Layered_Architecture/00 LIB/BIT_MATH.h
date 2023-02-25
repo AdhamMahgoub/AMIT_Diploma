8c198693c4c5e8635bb93216f9580479c97deb80
Adham Mahgoub <adhammahgoub1@gmail.com>
2023-02-25 16:46:30 +0200

    removed explanation comments

--- Updated_Layered_Architecture/00 LIB/BIT_MATH.h
+++ Updated_Layered_Architecture/00 LIB/BIT_MATH.h
@@ -1,19 +1,6 @@
 #ifndef 	BIT_MATH_H
 #define 	BIT_MATH_H
 
-
-/* Explanation
-SET 	= 1
-CLEAR 	= 0
-TOGGLE	= Reverse Current
-GET		= Read input
-*/
-
-/*			SET		particular Bit 				*/
-/* 			CLEAR	particular Bit 				*/
-/* 			TOGGLE	particular Bit 				*/
-/* 			GET		particular Bit 				*/
-
 #define 	SET_BIT(REG,BIT)		REG |= (1<<BIT)
 #define 	CLR_BIT(REG,BIT)		REG &= ~(1<<BIT)
 #define 	TOG_BIT(REG,BIT)		REG ^= (1<<BIT)
