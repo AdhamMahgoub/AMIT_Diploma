0047665a4d98bcb3e126701557766eda7d2dcd5c
AdhamMahgoub <120437302+AdhamMahgoub@users.noreply.github.com>
2023-02-25 16:56:00 +0200

    Update STD_TYPES.h

--- Updated_Layered_Architecture/00 LIB/STD_TYPES.h
+++ Updated_Layered_Architecture/00 LIB/STD_TYPES.h
@@ -2,19 +2,19 @@
 #define STD_TYPES_H
 
 
-typedef unsigned 				char 	u8; 
+typedef unsigned 			char 	u8; 
 typedef unsigned 	short 		int  	u16; 
-typedef unsigned 				int  	u32; 
+typedef unsigned 			int  	u32; 
 typedef unsigned 	long		int  	u64; 
 		
-typedef signed 					char 	s8; 
+typedef signed 				char 	s8; 
 typedef signed 		short 		int  	s16; 
-typedef signed 					int  	s32; 
+typedef signed 				int  	s32; 
 typedef signed 		long		int  	s64; 
 		
-typedef 						float  	f32; 
-typedef  						double 	f64; 
-typedef 			long 		double 	f128; 
+typedef 				float  	f32; 
+typedef  				double 	f64; 
+typedef 		long 		double 	f128; 
 
 
 
