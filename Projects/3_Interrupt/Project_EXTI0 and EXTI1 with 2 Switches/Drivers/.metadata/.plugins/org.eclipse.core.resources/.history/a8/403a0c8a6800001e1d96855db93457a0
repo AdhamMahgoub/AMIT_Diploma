6666b40f62f232d982b98b7ad96835cf7fe7eab2
Adham Mahgoub <adhammahgoub1@gmail.com>
2023-06-03 21:58:33 +0300

    added project to the EXTI

---
+++ Projects/3_Interrupt/Project_EXTI0 and EXTI1 with 2 Switches/Drivers/.metadata/.plugins/org.eclipse.core.resources/.history/a8/403a0c8a6800001e1d96855db93457a0
@@ -0,0 +1,11 @@
+#ifndef UART_interface_h
+#define UART_interface_h
+
+void UART_Init (void);
+void UART_SendByte (u8 Byte);
+u8 UART_RecieveByte (void);
+void UART_send_string(const char *arr);
+void UART_send_float(float x);
+
+
+#endif
---
+++ Projects/3_Interrupt/Project_EXTI0 and EXTI1 with 2 Switches/Drivers/Mastering_Debugging/MCAL/Communication/SPI/SPI_prog.c
@@ -0,0 +1,74 @@
+#include "../../../LIB/STD_TYPES.H"
+#include "../../../LIB/BIT_MATH.h"
+
+#include "../../DIO/DIO_interface.h"
+
+#include "SPI_interface.h"
+#include "SPI_private.h"
+#include "../../EXTI/EXTI_private.h"	//	For Global Interrupt Enable (SREG)
+
+
+void	SPI_initMaster(void)
+{
+	//	Configure SPI Pins
+	DIO_voidSetPinDirection(PORTB, SS_bit,		OUTPUT);	//	SS Pin Output
+	DIO_voidSetPinDirection(PORTB, MOSI_bit,	OUTPUT);	//	MOSI Pin Output
+	DIO_voidSetPinDirection(PORTB, MISO_bit,	INPUT);		//	MISO Pin Input
+	DIO_voidSetPinDirection(PORTB, SCK_bit,		OUTPUT);	//	SCK Pin Output
+
+	//	Master Mode
+	SET_BIT(SPCR, MSTR_bit);	//	Master/Slave Select	(THIS MUST BE BEFORE THE SPI ENABLE)
+	
+	//	Enable SPI
+	SET_BIT(SPCR, SPE_bit);		//	SPI Enable
+	
+	//	Global Interrupt Enable
+	SET_BIT(SREG, 7);
+	
+	// 	SPI Interrupt Enable
+	SET_BIT(SPCR, SPIE_bit);
+
+	/*			The Pins that are not written are Zeroes by Default		*/
+	
+}
+
+void	SPI_initSlave(void)
+{
+	//	Configure SPI Pins
+	DIO_voidSetPinDirection(PORTB, SS_bit,		INPUT);		//	SS Pin INPUT
+	DIO_voidSetPinDirection(PORTB, MOSI_bit,	INPUT);		//	MOSI Pin INPUT
+	DIO_voidSetPinDirection(PORTB, MISO_bit,	OUTPUT);	//	MISO Pin OUTPUT
+	DIO_voidSetPinDirection(PORTB, SCK_bit,		INPUT);		//	SCK Pin INPUT
+
+	//	Enable SPI
+	SET_BIT(SPCR, SPE_bit);			//	SPI Enable
+	
+	
+	//	Global Interrupt Enable
+	SET_BIT(SREG, 7);
+	
+	// 	SPI Interrupt Enable
+	SET_BIT(SPCR, SPIE_bit);
+	
+	/*			The Pins that are not written are Zeroes		*/
+}
+
+void	SPI_sendByte(u8 data)
+{
+	//	Send Data by SPI
+	SPDR = data;
+	
+	//	Wait until SPI Interrupt Flag = 1 (data sent correctly)
+	while(GET_BIT(SPSR, SPIF_bit)==0);
+}
+
+u8		SPI_recieveByte(void)
+{
+	//	Wait until SPI Interrupt Flag = 1 (data received correctly)
+	while(GET_BIT(SPSR, SPIF_bit)==0);
+	
+	// Return the Receive Byte from SPI data Register
+	return(SPDR);
+}
+
+
