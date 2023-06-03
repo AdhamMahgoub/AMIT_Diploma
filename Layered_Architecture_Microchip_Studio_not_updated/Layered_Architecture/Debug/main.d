773ed3ecafc37b157582176dfa83a0a8693a0886
Adham Mahgoub <adhammahgoub1@gmail.com>
2023-06-03 13:01:12 +0300

    working version of debugging using microchip + eclipse (with proteus)

--- Layered_Architecture_Microchip_Studio_not_updated/Layered_Architecture/Debug/main.d
+++ Layered_Architecture_Microchip_Studio_not_updated/Layered_Architecture/Debug/main.d
@@ -1,11 +1,13 @@
 main.d main.o: .././main.c .././LIB/STD_TYPES.h .././LIB/BIT_MATH.h \
  .././MCAL/Communication/UART/UART_interface.h \
- .././MCAL/DIO/DIO_private.h
+ .././MCAL/DIO/DIO_interface.h .././MCAL/DIO/DIO_private.h
 
 .././LIB/STD_TYPES.h:
 
 .././LIB/BIT_MATH.h:
 
 .././MCAL/Communication/UART/UART_interface.h:
+
+.././MCAL/DIO/DIO_interface.h:
 
 .././MCAL/DIO/DIO_private.h:
--- Layered_Architecture_Microchip_Studio_not_updated/Layered_Architecture/main.c
+++ Layered_Architecture_Microchip_Studio_not_updated/Layered_Architecture/main.c
@@ -4,20 +4,18 @@
 #include "LIB/BIT_MATH.h"
 
 #include "MCAL/Communication/UART/UART_interface.h"
-
-
+#include "MCAL/DIO/DIO_interface.h"
 #include "MCAL/DIO/DIO_private.h"
+
+#ifdef F_CPU
+#undef F_CPU
+#define F_CPU 8000000
+#endif
 
 int main(void)
 {
-	
-	
-		
-				
-	while(1)
-    {	
-			 	
 
-	}
-	
+	UART_Init();
+	UART_send_string("Adham Mahgoub");
+
 }
